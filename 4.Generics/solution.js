ff7bf9350ca0a210c97819288a21b020a6a45bcf
djurkewindowssch <djurkesajt@gmail.com>
2026-03-17 11:51:00 +0100

    added objects, odgovorNaBlog and odgovorNaBlogNiz

--- 4.Generics/solution.js
+++ 4.Generics/solution.js
@@ -10,3 +10,52 @@ function ukloniDuplikate(niz) {
     return Array.from(new Set(niz));
 }
 console.log(ukloniDuplikate([1, 2, 1, 5, 6, 2, 2, 4, 1, 3, 5, 7, 9]));
+var odgovorNaBlog = {
+    success: true,
+    data: {
+        ID: Math.floor(Math.random() * 100),
+        naslov: 'Suboticke ulice',
+        sadrzaj: 'Suboticke ulice su prelepe...',
+        brojPregleda: 15000,
+        ocena: null,
+        autor: {
+            ime: 'Djurke',
+            email: 'djurkeceo@me.com',
+        },
+        statusObjave: 'draft'
+    },
+    error: '500'
+};
+var odgovorNaBlogNiz = {
+    success: true,
+    data: [{
+            ID: Math.floor(Math.random() * 100),
+            naslov: 'Suboticke ulice 1',
+            sadrzaj: 'Suboticke ulice 1 su prelepe...',
+            brojPregleda: 1400,
+            ocena: null,
+            autor: {
+                ime: 'Djurke1',
+                email: 'djurkeceo1@me.com',
+            },
+            statusObjave: 'published'
+        },
+        {
+            ID: Math.floor(Math.random() * 100),
+            naslov: 'Suboticke ulice 2',
+            sadrzaj: 'Suboticke ulice 2 su prelepe...',
+            brojPregleda: 2500,
+            ocena: null,
+            autor: {
+                ime: 'Djurke2',
+                email: 'djurkeceo2@me.com',
+            },
+            statusObjave: 'archived'
+        }],
+    error: '500'
+};
+// function kreitajOdgovor<T>(data: T): ApiOdgovor<T> {
+//     return 
+// }
+console.log(odgovorNaBlog);
+console.log(odgovorNaBlogNiz.data[0]);
--- 4.Generics/solution.ts
+++ 4.Generics/solution.ts
@@ -13,6 +13,10 @@ interface BlogPost {
     statusObjave: StatusObjave;
 }
 
+interface BlogPostWithID extends BlogPost {
+    ID: number;
+}
+
 type StatusObjave = "draft" | "published" | "archived"
 
 function poslednji<T> (elniza: T[]): T | null {
@@ -28,3 +32,63 @@ function ukloniDuplikate<T> (niz: T[]): T[] {
 }
 
 console.log(ukloniDuplikate([1, 2, 1, 5, 6, 2, 2, 4, 1, 3, 5, 7, 9]))
+
+interface ApiOdgovor<T> {
+    success: boolean;
+    data: T;
+    error?: string | null;
+}
+
+const odgovorNaBlog: ApiOdgovor<BlogPostWithID> = {
+    success: true,
+    data: {
+        ID: Math.floor(Math.random() * 100),
+        naslov: 'Suboticke ulice',
+        sadrzaj: 'Suboticke ulice su prelepe...',
+        brojPregleda: 15000,
+        ocena: null,
+        autor: {
+            ime: 'Djurke',
+            email: 'djurkeceo@me.com',
+        },
+        statusObjave: 'draft'
+    },
+    error: '500'
+}
+
+const odgovorNaBlogNiz: ApiOdgovor<BlogPostWithID[]> = {
+    success: true,
+    data: [{
+        ID: Math.floor(Math.random() * 100),
+        naslov: 'Suboticke ulice 1',
+        sadrzaj: 'Suboticke ulice 1 su prelepe...',
+        brojPregleda: 1400,
+        ocena: null,    
+        autor: {
+            ime: 'Djurke1',
+            email: 'djurkeceo1@me.com',
+        },
+        statusObjave: 'published'
+    },
+    {
+        ID: Math.floor(Math.random() * 100),
+        naslov: 'Suboticke ulice 2',
+        sadrzaj: 'Suboticke ulice 2 su prelepe...',
+        brojPregleda: 2500,
+        ocena: null,    
+        autor: {
+            ime: 'Djurke2',
+            email: 'djurkeceo2@me.com',
+        },
+        statusObjave: 'archived'
+    }],
+    error: '500'
+}
+
+// function kreitajOdgovor<T>(data: T): ApiOdgovor<T> {
+//     return 
+// }
+
+
+console.log(odgovorNaBlog)
+console.log(odgovorNaBlogNiz)
