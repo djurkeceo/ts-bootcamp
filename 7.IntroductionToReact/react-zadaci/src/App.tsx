dd60564e8abdaf6c93822fbc75da6893d5049168
djurkefedora <djurkesajt@gmail.com>
2026-03-23 15:11:43 +0100

    added the counting function to app.tsx

--- 7.IntroductionToReact/react-zadaci/src/App.tsx
+++ 7.IntroductionToReact/react-zadaci/src/App.tsx
@@ -1,9 +1,25 @@
-// Svaka React komponenta je funkcija koja vraća JSX
+import { useState } from 'react'
+
+function Brojac() {
+    // [trenutnaVrednost, funkcijaZaPromenu] = useState(pocetnaVrednost)
+    const [broj, setBroj] = useState<number>(0)
+
+    return (
+        <div>
+            <p>Broj: {broj}</p>
+            <button onClick={() => setBroj(broj + 1)}>Povećaj</button>
+            <button onClick={() => setBroj(broj - 1)}>Smanji</button>
+        </div>
+    )
+}
+
 function App() {
     return (
         <div>
             <h1>Zdravo iz Reacta!</h1>
             <p>Ovo je moja prva komponenta.</p>
+
+            <Brojac />
         </div>
     )
 }
